25f3b310df191b6b3db9a4a8c7cfd760f1c9a169
trbrooks <tim.r.brooks@nasa.gov>
2022-07-28 08:55:09 -0400

    adding solutions

--- Example 4/crm_resonance.py
+++ Example 4/crm_resonance.py
@@ -21,14 +21,13 @@ bdfFile = os.path.join(os.path.dirname(__file__), '../input files/CRM_box_2nd.bd
 FEAAssembler = pyTACS(bdfFile, comm=comm)
 
 # Material properties
-rho = 2780.0  # density kg/m^3
-E = 73.1e9  # Young's modulus (Pa)
-nu = 0.33  # Poisson's ratio
-ys = 324.0e6  # yield stress
+rho = 2780.0        # density kg/m^3
+E = 73.1e9          # Young's modulus (Pa)
+nu = 0.33           # Poisson's ratio
+ys = 324.0e6        # yield stress
 
 # Shell thickness
-t = 0.01  # m
-
+t = 0.01            # m
 
 # Callback function used to setup TACS element objects and DVs
 def elemCallBack(dvNum, compID, compDescript, elemDescripts, globalDVs, **kwargs):
@@ -44,6 +43,37 @@ def elemCallBack(dvNum, compID, compDescript, elemDescripts, globalDVs, **kwargs
 
     return elem
 
-
 # Set up elements and TACS assembler
 FEAAssembler.initialize(elemCallBack)
+
+modalProb = FEAAssembler.createModalProblem("CRMModes", 36.0, 10)
+modalProb.solve()
+nmodes = modalProb.getNumEigs()
+for mode_i in range(nmodes):
+    eigVal, eigVector = modalProb.getVariables(mode_i)
+    f_hz = np.sqrt(eigVal)/2/np.pi
+    print(f"mode {mode_i}: {f_hz} hz")
+modalProb.writeSolution()
+
+modalProb.solve()
+modalProb.writeSolution()
+
+numEigs = modalProb.getNumEigs()
+
+for i in range(numEigs):
+    eigVal, eigVec = modalProb.getVariables(i)
+    f_hz = np.sqrt(eigVal) / 2 / np.pi
+    print(f"freq {i+1}: {f_hz}")
+
+transientProb = FEAAssembler.createTransientProblem("CRMResonance", 0.0, 5.0, 100, options={"printLevel": 2})
+
+eigVal0, eigVec0 = modalProb.getVariables(0)
+eigFreq0 = np.sqrt(eigVal)
+
+timeSteps = transientProb.getTimeSteps()
+for step_i, t in enumerate(timeSteps):
+    F = 1e3 * eigVec0 * np.sin(eigFreq0*t)
+    transientProb.addLoadToRHS(step_i, F)
+
+transientProb.solve()
+transientProb.writeSolution()
--- Example 5/beam_opt.py
+++ Example 5/beam_opt.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 from tacs import pyTACS, constitutive, elements, functions
+from pyoptsparse import OPT, Optimization
 
 bdfFile = os.path.join(os.path.dirname(__file__), '../input files/pinned_beam.bdf')
 FEAAssembler = pyTACS(bdfFile)
@@ -34,6 +35,68 @@ F = np.zeros(6)
 F[1] = 1e2
 staticProblem.addLoadToNodes(27, F, nastranOrdering=True)
 
+# Create transient prob
+transientProb = FEAAssembler.createTransientProblem("Case2", 0.0, 0.5, 50)
+timeSteps = transientProb.getTimeSteps()
+T = np.zeros(3)
+for step_i, t in enumerate(timeSteps):
+    T[1] = 3.5e2 * np.sin(2 * np.pi * 12.7 * t)
+    compIDs = FEAAssembler.selectCompIDs()
+    transientProb.addTractionToComponents(step_i, compIDs, T)
+allProbs = [staticProblem, transientProb]
+
 # Add eval functions
-staticProblem.addFunction("mass", functions.StructuralMass)
-staticProblem.addFunction("ks_failure", functions.KSFailure, ksWeight=100.0)
+for problem in allProbs:
+    problem.addFunction("mass", functions.StructuralMass)
+    problem.addFunction("ks_failure", functions.KSFailure, ksWeight=100.0)
+
+
+def objfunc(xdict):
+    funcs = {}
+    for problem in allProbs:
+        problem.setDesignVars(xdict)
+        problem.solve()
+        problem.evalFunctions(funcs)
+    staticProblem.writeSolution()
+    print(funcs)
+    return funcs
+
+
+def sens(xdict, funcs):
+    funcsSens = {}
+    for problem in allProbs:
+        problem.setDesignVars(xdict)
+        problem.solve()
+        problem.evalFunctionsSens(funcsSens)
+    for func in funcsSens:
+        funcsSens[func].pop("Xpts")
+    return funcsSens
+
+
+# Optimization Object
+optProb = Optimization("Beam Optimization Problem", objfunc)
+
+# Design Variables
+ndvs = FEAAssembler.getNumDesignVars()
+optProb.addVarGroup("struct", ndvs, lower=1e-3, upper=8e-3, value=8e-3, scale=1e3)
+
+# Constraints
+optProb.addConGroup("Case1_ks_failure", 1, upper=1.0)
+optProb.addConGroup("Case2_ks_failure", 1, upper=1.0, scale=0.1)
+
+# Objective
+optProb.addObj("Case1_mass")
+
+# Check optimization problem:
+print(optProb)
+
+# Optimizer
+opt = OPT("SNOPT", options={"Major step limit": 1e-1,
+                            "Penalty parameter": 1.0})
+
+# Solution
+sol = opt(optProb, sens=sens)
+
+# Check Solution
+print(sol)
+transientProb.writeSolution()
